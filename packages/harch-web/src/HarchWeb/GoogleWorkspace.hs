b43e5f2c1ebe59911a8e293c888fc90f7a705be3
David Bruck <1393236+gorban@users.noreply.github.com>
2026-07-19 01:53:29 -0500

    Add Google Workspace JWT token provider

---
+++ packages/harch-web/src/HarchWeb/GoogleWorkspace.hs
@@ -0,0 +1,279 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HarchWeb.GoogleWorkspace
+  ( GoogleWorkspaceServiceAccount,
+    GoogleWorkspaceClock,
+    decodeGoogleWorkspaceServiceAccount,
+    gmailSendScope,
+    mkGoogleWorkspaceAccessTokenProvider,
+  )
+where
+
+import Control.Monad (unless)
+import Crypto.Hash.Algorithms (SHA256 (..))
+import Crypto.Number.Serialize (i2osp)
+import Crypto.PubKey.RSA.PKCS15 qualified as RSA
+import Crypto.PubKey.RSA.Types qualified as RSA
+import Data.Aeson qualified as Aeson
+import Data.Aeson.Types qualified as AesonTypes
+import Data.ByteString (ByteString)
+import Data.ByteString qualified as ByteString
+import Data.ByteString.Base64 qualified as Base64
+import Data.ByteString.Lazy qualified as LazyByteString
+import Data.PEM qualified as PEM
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Encoding qualified as TextEncoding
+import Data.Time (UTCTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+import Data.Word (Word8)
+import HarchWeb.Gmail
+  ( GmailAccessTokenProvider,
+    GmailHttpRequest (..),
+    GmailHttpResponse (..),
+    GmailHttpRunner,
+  )
+
+data GoogleWorkspaceServiceAccount = GoogleWorkspaceServiceAccount
+  { serviceAccountClientEmail :: Text,
+    serviceAccountPrivateKeyPem :: Text,
+    serviceAccountImpersonatedEmail :: Text
+  }
+
+type GoogleWorkspaceClock = IO UTCTime
+
+gmailSendScope :: Text
+gmailSendScope = "https://www.googleapis.com/auth/gmail.send"
+
+decodeGoogleWorkspaceServiceAccount :: Text -> Text -> Either Text GoogleWorkspaceServiceAccount
+decodeGoogleWorkspaceServiceAccount encodedCredentials impersonatedEmail = do
+  validImpersonatedEmail <- ensureNonEmpty "Google Workspace impersonated email must not be empty" impersonatedEmail
+  decodedCredentials <- mapFailure "Google Workspace credentials must be base64-encoded JSON" (Base64.decode (TextEncoding.encodeUtf8 encodedCredentials))
+  credentials <- mapFailure "Google Workspace credentials must contain client_email and private_key" (Aeson.eitherDecodeStrict decodedCredentials :: Either String Aeson.Value)
+  (clientEmail, privateKeyPem) <- serviceAccountFields credentials
+  validClientEmail <- ensureNonEmpty "Google Workspace credentials must contain a nonempty client_email" clientEmail
+  validPrivateKeyPem <- ensureNonEmpty "Google Workspace credentials must contain a nonempty private_key" privateKeyPem
+  pure
+    GoogleWorkspaceServiceAccount
+      { serviceAccountClientEmail = validClientEmail,
+        serviceAccountPrivateKeyPem = validPrivateKeyPem,
+        serviceAccountImpersonatedEmail = validImpersonatedEmail
+      }
+
+serviceAccountFields :: Aeson.Value -> Either Text (Text, Text)
+serviceAccountFields value =
+  case value of
+    Aeson.Object object ->
+      case (AesonTypes.parseMaybe (Aeson..: "client_email") object, AesonTypes.parseMaybe (Aeson..: "private_key") object) of
+        (Just clientEmail, Just privateKeyPem) -> Right (clientEmail, privateKeyPem)
+        _ -> Left "Google Workspace credentials must contain client_email and private_key"
+    _ -> Left "Google Workspace credentials must contain client_email and private_key"
+
+mkGoogleWorkspaceAccessTokenProvider :: GmailHttpRunner -> GoogleWorkspaceClock -> GoogleWorkspaceServiceAccount -> GmailAccessTokenProvider
+mkGoogleWorkspaceAccessTokenProvider runRequest currentTime serviceAccount = do
+  now <- currentTime
+  assertion <- eitherToIoError (signedAssertion serviceAccount now)
+  response <-
+    runRequest
+      GmailHttpRequest
+        { gmailHttpMethod = "POST",
+          gmailHttpUrl = "https://oauth2.googleapis.com/token",
+          gmailHttpHeaders = [("Content-Type", "application/x-www-form-urlencoded")],
+          gmailHttpBody = "grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion=" <> assertion
+        }
+  unless (gmailHttpStatus response >= 200 && gmailHttpStatus response < 300) $
+    ioError (userError ("Google Workspace token exchange failed with status " <> show (gmailHttpStatus response)))
+  accessToken <- eitherToIoError (accessTokenFromResponse (gmailHttpResponseBody response))
+  unless (validAccessToken accessToken) $
+    ioError (userError "Google Workspace token exchange returned an invalid access token")
+  pure accessToken
+
+signedAssertion :: GoogleWorkspaceServiceAccount -> UTCTime -> Either Text Text
+signedAssertion serviceAccount now = do
+  let issuedAt = floor (utcTimeToPOSIXSeconds now) :: Integer
+      header = base64Url "{\"alg\":\"RS256\",\"typ\":\"JWT\"}"
+      claims =
+        base64Url
+          ( LazyByteString.toStrict
+              ( Aeson.encode
+                  ( Aeson.object
+                      [ "iss" Aeson..= serviceAccountClientEmail serviceAccount,
+                        "scope" Aeson..= gmailSendScope,
+                        "aud" Aeson..= ("https://oauth2.googleapis.com/token" :: Text),
+                        "exp" Aeson..= (issuedAt + 3600),
+                        "iat" Aeson..= issuedAt,
+                        "sub" Aeson..= serviceAccountImpersonatedEmail serviceAccount
+                      ]
+                  )
+              )
+          )
+      signingInput = header <> "." <> claims
+  signature <- signRs256 (serviceAccountPrivateKeyPem serviceAccount) (TextEncoding.encodeUtf8 signingInput)
+  pure (signingInput <> "." <> base64Url signature)
+
+signRs256 :: Text -> ByteString -> Either Text ByteString
+signRs256 pem signingInput = do
+  privateKey <- rsaPrivateKeyFromPem pem
+  forceRsaPrivateKey privateKey `seq`
+    case RSA.sign Nothing (Just $! SHA256) privateKey signingInput of
+      Left _ -> Left "Google Workspace private key could not sign the JWT"
+      Right signature -> ByteString.length signature `seq` Right signature
+{-# ANN signRs256 ("HLint: ignore Redundant $!" :: String) #-}
+
+rsaPrivateKeyFromPem :: Text -> Either Text RSA.PrivateKey
+rsaPrivateKeyFromPem pem = do
+  parsedPem <- mapFailure "Google Workspace private key is not valid PEM" (PEM.pemParseBS (TextEncoding.encodeUtf8 pem))
+  der <-
+    case parsedPem of
+      [privateKey] ->
+        case PEM.pemName privateKey of
+          "PRIVATE KEY" -> Right (PEM.pemContent privateKey)
+          _ -> Left "Google Workspace private key must contain one PKCS#8 PRIVATE KEY block"
+      _ -> Left "Google Workspace private key must contain one PKCS#8 PRIVATE KEY block"
+  parsedKey <- maybeToEither "Google Workspace private key is not valid PKCS#8" (decodeDer der)
+  pkcs1Der <-
+    case parsedKey of
+      [DerSequence [DerInteger 0, DerSequence [DerObjectIdentifier rsaEncryptionOid, DerNull], DerOctetString encodedKey]]
+        | rsaEncryptionOid == rsaEncryptionObjectIdentifier -> Right encodedKey
+      _ -> Left "Google Workspace private key must be an RSA PKCS#8 key"
+  pkcs1 <- maybeToEither "Google Workspace private key is not valid RSA data" (decodeDer pkcs1Der)
+  case pkcs1 of
+    [DerSequence [DerInteger 0, DerInteger modulus, DerInteger publicExponent, DerInteger privateExponent, DerInteger primeOne, DerInteger primeTwo, DerInteger exponentOne, DerInteger exponentTwo, DerInteger coefficient]] ->
+      modulus `seq`
+        publicExponent `seq`
+          privateExponent `seq`
+            primeOne `seq`
+              primeTwo `seq`
+                exponentOne `seq`
+                  exponentTwo `seq`
+                    coefficient `seq`
+                      Right
+                        ( RSA.PrivateKey
+                            (RSA.PublicKey (ByteString.length (i2osp modulus)) modulus publicExponent)
+                            privateExponent
+                            primeOne
+                            primeTwo
+                            exponentOne
+                            exponentTwo
+                            coefficient
+                        )
+    _ -> Left "Google Workspace private key must contain an RSA private key"
+
+forceRsaPrivateKey :: RSA.PrivateKey -> ()
+forceRsaPrivateKey privateKey =
+  RSA.public_size (RSA.private_pub privateKey) `seq`
+    RSA.public_n (RSA.private_pub privateKey) `seq`
+      RSA.public_e (RSA.private_pub privateKey) `seq`
+        RSA.private_d privateKey `seq`
+          RSA.private_p privateKey `seq`
+            RSA.private_q privateKey `seq`
+              RSA.private_dP privateKey `seq`
+                RSA.private_dQ privateKey `seq`
+                  RSA.private_qinv privateKey `seq`
+                    ()
+
+rsaEncryptionObjectIdentifier :: ByteString
+rsaEncryptionObjectIdentifier = "\x2a\x86\x48\x86\xf7\x0d\x01\x01\x01"
+
+data DerValue
+  = DerSequence [DerValue]
+  | DerInteger Integer
+  | DerObjectIdentifier ByteString
+  | DerNull
+  | DerOctetString ByteString
+
+decodeDer :: ByteString -> Maybe [DerValue]
+decodeDer value =
+  if ByteString.null value
+    then Nothing
+    else parseDerValues value
+
+parseDerValues :: ByteString -> Maybe [DerValue]
+parseDerValues value =
+  if ByteString.null value
+    then Just []
+    else do
+      (derValue, remaining) <- parseDerValue value
+      values <- parseDerValues remaining
+      pure (derValue : values)
+
+parseDerValue :: ByteString -> Maybe (DerValue, ByteString)
+parseDerValue value = do
+  (tag, afterTag) <- takeDerByte value
+  (contentLength, afterLength) <- parseDerLength afterTag
+  (content, remaining) <- takeDerBytes contentLength afterLength
+  derValue <-
+    case tag of
+      2 -> Just (DerInteger (ByteString.foldl' appendDerByte 0 content))
+      4 -> Just (DerOctetString content)
+      5 -> Just DerNull
+      6 -> Just (DerObjectIdentifier content)
+      48 -> DerSequence <$> decodeDer content
+      _ -> Nothing
+  pure (derValue, remaining)
+
+parseDerLength :: ByteString -> Maybe (Int, ByteString)
+parseDerLength value = do
+  (firstByte, remaining) <- takeDerByte value
+  if firstByte < 128
+    then Just (fromIntegral firstByte, remaining)
+    else do
+      (lengthBytes, afterLength) <- takeDerBytes (fromIntegral (firstByte - 128)) remaining
+      pure (ByteString.foldl' appendDerLengthByte 0 lengthBytes, afterLength)
+
+takeDerByte :: ByteString -> Maybe (Word8, ByteString)
+takeDerByte = ByteString.uncons
+
+takeDerBytes :: Int -> ByteString -> Maybe (ByteString, ByteString)
+takeDerBytes contentLength value =
+  if contentLength <= ByteString.length value
+    then Just (ByteString.splitAt contentLength value)
+    else Nothing
+
+appendDerByte :: Integer -> Word8 -> Integer
+appendDerByte total byte = total * 256 + fromIntegral byte
+
+appendDerLengthByte :: Int -> Word8 -> Int
+appendDerLengthByte total byte = total * 256 + fromIntegral byte
+
+accessTokenFromResponse :: Text -> Either Text Text
+accessTokenFromResponse response = do
+  value <- mapFailure "Google Workspace token exchange returned invalid JSON" (Aeson.eitherDecodeStrict (TextEncoding.encodeUtf8 response) :: Either String Aeson.Value)
+  case value of
+    Aeson.Object object ->
+      case AesonTypes.parseMaybe (Aeson..: "access_token") object of
+        Just accessToken -> Right accessToken
+        Nothing -> Left "Google Workspace token exchange response did not contain access_token"
+    _ -> Left "Google Workspace token exchange returned invalid JSON"
+
+base64Url :: ByteString -> Text
+base64Url =
+  TextEncoding.decodeUtf8
+    . ByteString.dropWhileEnd (== 61)
+    . ByteString.map replaceBase64Character
+    . Base64.encode
+  where
+    replaceBase64Character character =
+      case character of
+        43 -> 45
+        47 -> 95
+        _ -> character
+
+ensureNonEmpty :: Text -> Text -> Either Text Text
+ensureNonEmpty message value =
+  case Text.uncons value of
+    Nothing -> Left message
+    Just _ -> Right value
+
+mapFailure :: Text -> Either error value -> Either Text value
+mapFailure message = either (const (Left message)) Right
+
+maybeToEither :: Text -> Maybe value -> Either Text value
+maybeToEither message = maybe (Left message) Right
+
+eitherToIoError :: Either Text value -> IO value
+eitherToIoError = either (ioError . userError . Text.unpack) pure
+
+validAccessToken :: Text -> Bool
+validAccessToken value =
+  not (Text.null value) && Text.all (\character -> character /= '\r' && character /= '\n') value
---
+++ packages/harch-web/test/Unit/HarchWeb/GoogleWorkspaceSpec.hs
@@ -0,0 +1,246 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Unit.HarchWeb.GoogleWorkspaceSpec (spec) where
+
+import Control.Exception (SomeException, displayException, try)
+import Crypto.Hash.Algorithms (SHA256 (..))
+import Crypto.PubKey.RSA qualified as RSA
+import Crypto.PubKey.RSA.PKCS15 qualified as RSAPKCS15
+import Data.ASN1.BinaryEncoding (DER (..))
+import Data.ASN1.Encoding (encodeASN1')
+import Data.ASN1.Types (ASN1 (..), ASN1ConstructionType (Sequence))
+import Data.Aeson qualified as Aeson
+import Data.ByteString qualified as ByteString
+import Data.ByteString.Base64 qualified as Base64
+import Data.ByteString.Lazy qualified as LazyByteString
+import Data.Either (fromLeft, fromRight)
+import Data.IORef (newIORef, readIORef, writeIORef)
+import Data.Maybe (fromMaybe)
+import Data.PEM qualified as PEM
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Text.Encoding qualified as TextEncoding
+import Data.Time (UTCTime (..), secondsToDiffTime)
+import Data.Time.Calendar (fromGregorian)
+import HarchWeb.Gmail (GmailHttpRequest (..), GmailHttpResponse (..))
+import HarchWeb.GoogleWorkspace
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "decodeGoogleWorkspaceServiceAccount" $ do
+    it "validates encoded credentials without exposing their contents" $ do
+      errorText (decodeGoogleWorkspaceServiceAccount "not-base64" "worker@example.test")
+        `shouldBe` "Google Workspace credentials must be base64-encoded JSON"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeBase64 "not json") "worker@example.test")
+        `shouldBe` "Google Workspace credentials must contain client_email and private_key"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeBase64 "{}") "worker@example.test")
+        `shouldBe` "Google Workspace credentials must contain client_email and private_key"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeBase64 "[]") "worker@example.test")
+        `shouldBe` "Google Workspace credentials must contain client_email and private_key"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeCredentials "" "key") "worker@example.test")
+        `shouldBe` "Google Workspace credentials must contain a nonempty client_email"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeCredentials "service@example.test" "") "worker@example.test")
+        `shouldBe` "Google Workspace credentials must contain a nonempty private_key"
+      errorText (decodeGoogleWorkspaceServiceAccount (encodeCredentials "service@example.test" "key") "")
+        `shouldBe` "Google Workspace impersonated email must not be empty"
+
+  describe "mkGoogleWorkspaceAccessTokenProvider" $ do
+    it "exchanges a signed domain-wide-delegation JWT for an access token" $ do
+      (encodedCredentials, publicKey) <- generatedCredentials
+      let serviceAccount = requiredEither (decodeGoogleWorkspaceServiceAccount encodedCredentials "mailer@example.test")
+      receivedRequest <- newIORef Nothing
+      let runner request = writeIORef receivedRequest (Just request) >> pure (GmailHttpResponse 200 "{\"access_token\":\"delegated-token\"}")
+      mkGoogleWorkspaceAccessTokenProvider runner fixedClock serviceAccount `shouldReturn` "delegated-token"
+      request <- required <$> readIORef receivedRequest
+      gmailHttpMethod request `shouldBe` "POST"
+      gmailHttpUrl request `shouldBe` "https://oauth2.googleapis.com/token"
+      gmailHttpHeaders request `shouldBe` [("Content-Type", "application/x-www-form-urlencoded")]
+      let assertion = Text.drop (Text.length jwtGrantPrefix) (gmailHttpBody request)
+          (header, claims, signature) = jwtParts assertion
+      decodeBase64Url header `shouldBe` "{\"alg\":\"RS256\",\"typ\":\"JWT\"}"
+      decodeBase64Url claims `shouldSatisfy` Text.isInfixOf "\"iss\":\"service@example.test\""
+      decodeBase64Url claims `shouldSatisfy` Text.isInfixOf "\"scope\":\"https://www.googleapis.com/auth/gmail.send\""
+      decodeBase64Url claims `shouldSatisfy` Text.isInfixOf "\"sub\":\"mailer@example.test\""
+      RSAPKCS15.verify (Just SHA256) publicKey (TextEncoding.encodeUtf8 (header <> "." <> claims)) (decodeBase64UrlBytes signature) `shouldBe` True
+
+    it "surfaces token failures without exposing the signed assertion" $ do
+      (encodedCredentials, _) <- generatedCredentials
+      let serviceAccount = requiredEither (decodeGoogleWorkspaceServiceAccount encodedCredentials "mailer@example.test")
+      let runner _ = pure (GmailHttpResponse 403 "domain-wide delegation is missing")
+      result <- try (mkGoogleWorkspaceAccessTokenProvider runner fixedClock serviceAccount) :: IO (Either SomeException Text)
+      case result of
+        Left failure -> do
+          displayException failure `shouldContain` "Google Workspace token exchange failed with status 403"
+          displayException failure `shouldNotContain` "BEGIN PRIVATE KEY"
+        Right _ -> expectationFailure "Expected token exchange to fail"
+
+    it "rejects failed token statuses before accepting their response bodies" $ do
+      (encodedCredentials, _) <- generatedCredentials
+      let serviceAccount = requiredEither (decodeGoogleWorkspaceServiceAccount encodedCredentials "mailer@example.test")
+      tooEarly <- tryStatus serviceAccount 199
+      rejected <- tryStatus serviceAccount 403
+      map displayException [tooEarly, rejected]
+        `shouldBe`
+          [ "user error (Google Workspace token exchange failed with status 199)",
+            "user error (Google Workspace token exchange failed with status 403)"
+          ]
+
+    it "rejects malformed and unsafe token responses" $ do
+      (encodedCredentials, _) <- generatedCredentials
+      let serviceAccount = requiredEither (decodeGoogleWorkspaceServiceAccount encodedCredentials "mailer@example.test")
+      malformed <- tryProvider serviceAccount "not json"
+      missing <- tryProvider serviceAccount "{}"
+      nonObject <- tryProvider serviceAccount "[]"
+      unsafe <- tryProvider serviceAccount "{\"access_token\":\"bad\\ntoken\"}"
+      map displayException [malformed, missing, nonObject, unsafe]
+        `shouldBe`
+          [ "user error (Google Workspace token exchange returned invalid JSON)",
+            "user error (Google Workspace token exchange response did not contain access_token)",
+            "user error (Google Workspace token exchange returned invalid JSON)",
+            "user error (Google Workspace token exchange returned an invalid access token)"
+          ]
+
+    it "rejects malformed PEM and RSA key encodings before sending a token request" $ do
+      let malformedPem = "-----BEGIN PRIVATE KEY-----\n%%%\n-----END PRIVATE KEY-----\n"
+          emptyPem = "\n"
+          wrongPemLabel = pemText "RSA PRIVATE KEY" ""
+          emptyPkcs8 = pemText "PRIVATE KEY" ""
+          invalidPkcs8 = pemText "PRIVATE KEY" "\x03\&\x00"
+          incompleteTag = pemText "PRIVATE KEY" "\x30"
+          incompleteContent = pemText "PRIVATE KEY" "\x30\x01"
+          nonRsaPkcs8 = pemText "PRIVATE KEY" (encodeASN1' DER [Null])
+          nonRsaOid = pemText "PRIVATE KEY" (pkcs8PrivateKeyWithOid [1, 2, 3] "")
+          invalidRsaData = pemText "PRIVATE KEY" (pkcs8PrivateKey "not DER")
+          malformedRsa = pemText "PRIVATE KEY" (pkcs8PrivateKey (encodeASN1' DER [Null]))
+          unsignableRsa = pemText "PRIVATE KEY" (pkcs8PrivateKey (rsaPrivateKeyDer 1 1 1 1 1 1 1 1))
+      failures <- mapM tryInvalidPrivateKey [malformedPem, emptyPem, wrongPemLabel, emptyPkcs8, invalidPkcs8, incompleteTag, incompleteContent, nonRsaPkcs8, nonRsaOid, invalidRsaData, malformedRsa, unsignableRsa]
+      map displayException failures
+        `shouldBe`
+          [ "user error (Google Workspace private key is not valid PEM)",
+            "user error (Google Workspace private key must contain one PKCS#8 PRIVATE KEY block)",
+            "user error (Google Workspace private key must contain one PKCS#8 PRIVATE KEY block)",
+            "user error (Google Workspace private key is not valid PKCS#8)",
+            "user error (Google Workspace private key is not valid PKCS#8)",
+            "user error (Google Workspace private key is not valid PKCS#8)",
+            "user error (Google Workspace private key is not valid PKCS#8)",
+            "user error (Google Workspace private key must be an RSA PKCS#8 key)",
+            "user error (Google Workspace private key must be an RSA PKCS#8 key)",
+            "user error (Google Workspace private key is not valid RSA data)",
+            "user error (Google Workspace private key must contain an RSA private key)",
+            "user error (Google Workspace private key could not sign the JWT)"
+          ]
+
+jwtGrantPrefix :: Text
+jwtGrantPrefix = "grant_type=urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Ajwt-bearer&assertion="
+
+fixedClock :: GoogleWorkspaceClock
+fixedClock = pure (UTCTime (fromGregorian 2026 1 2) (secondsToDiffTime 3))
+
+tryProvider :: GoogleWorkspaceServiceAccount -> Text -> IO SomeException
+tryProvider serviceAccount body = do
+  result <- try (mkGoogleWorkspaceAccessTokenProvider (const (pure (GmailHttpResponse 200 body))) fixedClock serviceAccount) :: IO (Either SomeException Text)
+  case result of
+    Left failure -> pure failure
+    Right _ -> expectationFailure "Expected token exchange to fail" >> error "unreachable"
+
+tryStatus :: GoogleWorkspaceServiceAccount -> Int -> IO SomeException
+tryStatus serviceAccount status = do
+  result <- try (mkGoogleWorkspaceAccessTokenProvider (const (pure (GmailHttpResponse status "response body is not exposed"))) fixedClock serviceAccount) :: IO (Either SomeException Text)
+  case result of
+    Left failure -> pure failure
+    Right _ -> expectationFailure "Expected token exchange to fail" >> error "unreachable"
+
+tryInvalidPrivateKey :: Text -> IO SomeException
+tryInvalidPrivateKey privateKeyPem = do
+  let serviceAccount = requiredEither (decodeGoogleWorkspaceServiceAccount (encodeCredentials "service@example.test" privateKeyPem) "mailer@example.test")
+  tryProvider serviceAccount "{\"access_token\":\"not-reached\"}"
+
+generatedCredentials :: IO (Text, RSA.PublicKey)
+generatedCredentials = do
+  (publicKey, privateKey) <- RSA.generate 1024 65537
+  let RSA.PrivateKey (RSA.PublicKey _ modulus publicExponent) privateExponent primeOne primeTwo exponentOne exponentTwo coefficient = privateKey
+      pkcs1 = rsaPrivateKeyDer modulus publicExponent privateExponent primeOne primeTwo exponentOne exponentTwo coefficient
+      pkcs8 = pkcs8PrivateKey pkcs1
+      pem = pemText "PRIVATE KEY" pkcs8
+  pure (encodeCredentials "service@example.test" pem, publicKey)
+
+encodeCredentials :: Text -> Text -> Text
+encodeCredentials clientEmail privateKey =
+  encodeBase64
+    ( LazyByteString.toStrict
+        (Aeson.encode (Aeson.object ["client_email" Aeson..= clientEmail, "private_key" Aeson..= privateKey]))
+    )
+
+pkcs8PrivateKey :: ByteString.ByteString -> ByteString.ByteString
+pkcs8PrivateKey = pkcs8PrivateKeyWithOid [1, 2, 840, 113549, 1, 1, 1]
+
+pkcs8PrivateKeyWithOid :: [Integer] -> ByteString.ByteString -> ByteString.ByteString
+pkcs8PrivateKeyWithOid objectIdentifier pkcs1 =
+  encodeASN1' DER
+    [ Start Sequence,
+      IntVal 0,
+      Start Sequence,
+      OID objectIdentifier,
+      Null,
+      End Sequence,
+      OctetString pkcs1,
+      End Sequence
+    ]
+
+rsaPrivateKeyDer :: Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> Integer -> ByteString.ByteString
+rsaPrivateKeyDer modulus publicExponent privateExponent primeOne primeTwo exponentOne exponentTwo coefficient =
+  encodeASN1' DER
+    [ Start Sequence,
+      IntVal 0,
+      IntVal modulus,
+      IntVal publicExponent,
+      IntVal privateExponent,
+      IntVal primeOne,
+      IntVal primeTwo,
+      IntVal exponentOne,
+      IntVal exponentTwo,
+      IntVal coefficient,
+      End Sequence
+    ]
+
+pemText :: String -> ByteString.ByteString -> Text
+pemText name content = TextEncoding.decodeUtf8 (PEM.pemWriteBS (PEM.PEM name [] content))
+
+encodeBase64 :: ByteString.ByteString -> Text
+encodeBase64 = TextEncoding.decodeUtf8 . Base64.encode
+
+decodeBase64Url :: Text -> Text
+decodeBase64Url = TextEncoding.decodeUtf8 . decodeBase64UrlBytes
+
+decodeBase64UrlBytes :: Text -> ByteString.ByteString
+decodeBase64UrlBytes value =
+  requiredEither
+    ( Base64.decode
+        ( TextEncoding.encodeUtf8
+            ( Text.map restoreBase64Character value
+                <> Text.replicate ((4 - Text.length value `mod` 4) `mod` 4) "="
+            )
+        )
+    )
+  where
+    restoreBase64Character character =
+      case character of
+        '-' -> '+'
+        '_' -> '/'
+        _ -> character
+
+jwtParts :: Text -> (Text, Text, Text)
+jwtParts assertion =
+  case Text.splitOn "." assertion of
+    [header, claims, signature] -> (header, claims, signature)
+    _ -> error "Expected a three-part JWT"
+
+required :: Maybe value -> value
+required = fromMaybe (error "Expected a valid Google Workspace test value")
+
+requiredEither :: Either error value -> value
+requiredEither = fromRight (error "Expected a valid Google Workspace test value")
+
+errorText :: Either Text value -> Text
+errorText = fromLeft "Expected credential validation to fail"
