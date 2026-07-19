c109fc6853e11c1f4035939283e33ba555b3dbdb
David Bruck <1393236+gorban@users.noreply.github.com>
2026-07-19 13:53:34 -0500

    Add hashed account recovery codes

---
+++ packages/harch-web/src/HarchWeb/RecoveryCode.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module HarchWeb.RecoveryCode
+  ( RecoveryCode,
+    RecoveryCodeHash,
+    generateRecoveryCode,
+    hashRecoveryCode,
+    hashRecoveryCodeWithSalt,
+    mkRecoveryCode,
+    readRecoveryCodeHash,
+    recoveryCodeHashText,
+    recoveryCodeText,
+    verifyRecoveryCode,
+  )
+where
+
+import Crypto.Random.Entropy (getEntropy)
+import Data.ByteString qualified as ByteString
+import Data.Char (isDigit)
+import Data.Text (Text)
+import Data.Text qualified as Text
+import Data.Word (Word8)
+import HarchWeb.Password
+  ( Password,
+    PasswordHash,
+    PasswordHashingPolicy,
+    hashPassword,
+    hashPasswordWithSalt,
+    mkPassword,
+    passwordHashText,
+    readPasswordHash,
+    verifyPassword,
+  )
+
+newtype RecoveryCode = RecoveryCode Text
+  deriving (Eq)
+
+newtype RecoveryCodeHash = RecoveryCodeHash PasswordHash
+
+generateRecoveryCode :: IO RecoveryCode
+generateRecoveryCode = RecoveryCode . renderCode <$> getEntropy recoveryCodeBytes
+
+mkRecoveryCode :: Text -> Maybe RecoveryCode
+mkRecoveryCode suppliedCode =
+  let normalizedCode = Text.toUpper (Text.filter (/= '-') suppliedCode)
+   in if Text.length normalizedCode == recoveryCodeHexCharacters && Text.all isHexDigit normalizedCode
+        then Just (RecoveryCode normalizedCode)
+        else Nothing
+
+recoveryCodeText :: RecoveryCode -> Text
+recoveryCodeText (RecoveryCode code) = Text.intercalate "-" (Text.chunksOf recoveryCodeGroupLength code)
+
+hashRecoveryCode :: PasswordHashingPolicy -> RecoveryCode -> IO (Maybe RecoveryCodeHash)
+hashRecoveryCode policy code = fmap (fmap RecoveryCodeHash) (hashPassword policy (passwordFor code))
+
+hashRecoveryCodeWithSalt :: PasswordHashingPolicy -> ByteString.ByteString -> RecoveryCode -> Maybe RecoveryCodeHash
+hashRecoveryCodeWithSalt policy salt code = RecoveryCodeHash <$> hashPasswordWithSalt policy salt (passwordFor code)
+
+verifyRecoveryCode :: RecoveryCode -> RecoveryCodeHash -> Bool
+verifyRecoveryCode code (RecoveryCodeHash storedHash) = verifyPassword (passwordFor code) storedHash
+
+recoveryCodeHashText :: RecoveryCodeHash -> Text
+recoveryCodeHashText (RecoveryCodeHash storedHash) = passwordHashText storedHash
+
+readRecoveryCodeHash :: Text -> Maybe RecoveryCodeHash
+readRecoveryCodeHash storedHash = RecoveryCodeHash <$> readPasswordHash storedHash
+
+passwordFor :: RecoveryCode -> Password
+passwordFor (RecoveryCode code) = mkPassword code
+
+renderCode :: ByteString.ByteString -> Text
+renderCode = Text.pack . concatMap renderByte . ByteString.unpack
+
+renderByte :: Word8 -> String
+renderByte byte = [hexDigit (byte `div` 16), hexDigit (byte `mod` 16)]
+
+hexDigit :: Word8 -> Char
+hexDigit value = "0123456789ABCDEF" !! fromIntegral value
+
+isHexDigit :: Char -> Bool
+isHexDigit character = isDigit character || ('A' <= character && character <= 'F')
+
+recoveryCodeBytes :: Int
+recoveryCodeBytes = 10
+
+recoveryCodeHexCharacters :: Int
+recoveryCodeHexCharacters = recoveryCodeBytes * 2
+
+recoveryCodeGroupLength :: Int
+recoveryCodeGroupLength = 5
---
+++ packages/harch-web/test/Unit/HarchWeb/RecoveryCodeSpec.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Unit.HarchWeb.RecoveryCodeSpec (spec) where
+
+import Data.ByteString qualified as ByteString
+import Data.Maybe (isNothing)
+import Data.Text qualified as Text
+import HarchWeb.Password (defaultPasswordHashingPolicy)
+import HarchWeb.RecoveryCode
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "RecoveryCode" $ do
+    it "canonicalizes user-entered hexadecimal codes and generates 80-bit codes" $ do
+      recoveryCodeText knownCode `shouldBe` "12345-6789A-BCDEF-01234"
+      knownCode == knownCode `shouldBe` True
+      knownCode /= otherCode `shouldBe` True
+      fmap recoveryCodeText (mkRecoveryCode "12345-6789a-bcdef-01234") `shouldBe` Just (recoveryCodeText knownCode)
+      fmap recoveryCodeText (mkRecoveryCode "123456789ABCDEF01234") `shouldBe` Just (recoveryCodeText knownCode)
+      map (isNothing . mkRecoveryCode) ["", "12345-6789A-BCDEF-0123", "12345-6789A-BCDEF-012345", "12345-6789A-BCDEF-0123G"] `shouldBe` [True, True, True, True]
+      generatedCode <- generateRecoveryCode
+      fmap recoveryCodeText (mkRecoveryCode (recoveryCodeText generatedCode)) `shouldBe` Just (recoveryCodeText generatedCode)
+
+  describe "RecoveryCodeHash" $ do
+    it "stores an Argon2id verifier without retaining the recovery code" $ do
+      let maybeHash = hashRecoveryCodeWithSalt defaultPasswordHashingPolicy (ByteString.replicate 16 1) knownCode
+          hashValue = required maybeHash
+      Text.isInfixOf "12345" (recoveryCodeHashText hashValue) `shouldBe` False
+      fmap recoveryCodeHashText (readRecoveryCodeHash (recoveryCodeHashText hashValue)) `shouldBe` Just (recoveryCodeHashText hashValue)
+      isNothing (readRecoveryCodeHash "not-an-argon2id-hash") `shouldBe` True
+      verifyRecoveryCode knownCode hashValue `shouldBe` True
+      verifyRecoveryCode otherCode hashValue `shouldBe` False
+      maybeRandomHash <- hashRecoveryCode defaultPasswordHashingPolicy knownCode
+      fmap (verifyRecoveryCode knownCode) maybeRandomHash `shouldBe` Just True
+
+knownCode :: RecoveryCode
+knownCode = required (mkRecoveryCode "12345-6789A-BCDEF-01234")
+
+otherCode :: RecoveryCode
+otherCode = required (mkRecoveryCode "12345-6789A-BCDEF-01235")
+
+required :: Maybe value -> value
+required maybeValue =
+  case maybeValue of
+    Just value -> value
+    Nothing -> error "expected a valid recovery-code value"
