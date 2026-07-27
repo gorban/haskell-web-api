05b6c046b54ad1e544e61d4e2fa975bddb0b01ba
David Bruck <1393236+gorban@users.noreply.github.com>
2026-07-26 19:51:20 -0500

    Aggregate independent recovery-code assertions

--- packages/harch-web/test/Unit/HarchWeb/RecoveryCodeSpec.hs
+++ packages/harch-web/test/Unit/HarchWeb/RecoveryCodeSpec.hs
@@ -4,22 +4,27 @@
 module Unit.HarchWeb.RecoveryCodeSpec (spec) where
 
 import Data.ByteString qualified as ByteString
+import Data.List.NonEmpty (NonEmpty (..))
 import Data.Maybe (isNothing)
 import Data.Text qualified as Text
 import HarchWeb.Password (defaultPasswordHashingPolicy)
 import HarchWeb.RecoveryCode
 import Test.Hspec
+import TestCore.CustomAssertions (expectAll)
 
 spec :: Spec
 spec = do
   describe "RecoveryCode" $ do
     it "canonicalizes user-entered hexadecimal codes and generates 80-bit codes" $ do
-      recoveryCodeText knownCode `shouldBe` "12345-6789A-BCDEF-01234"
-      knownCode == knownCode `shouldBe` True
-      knownCode /= otherCode `shouldBe` True
-      fmap recoveryCodeText (mkRecoveryCode "12345-6789a-bcdef-01234") `shouldBe` Just (recoveryCodeText knownCode)
-      fmap recoveryCodeText (mkRecoveryCode "123456789ABCDEF01234") `shouldBe` Just (recoveryCodeText knownCode)
-      map (isNothing . mkRecoveryCode) ["", "12345-6789A-BCDEF-0123", "12345-6789A-BCDEF-012345", "12345-6789A-BCDEF-0123G"] `shouldBe` [True, True, True, True]
+      expectAll
+        ( (recoveryCodeText knownCode `shouldBe` "12345-6789A-BCDEF-01234")
+            :| [ knownCode == knownCode `shouldBe` True,
+                 knownCode /= otherCode `shouldBe` True,
+                 fmap recoveryCodeText (mkRecoveryCode "12345-6789a-bcdef-01234") `shouldBe` Just (recoveryCodeText knownCode),
+                 fmap recoveryCodeText (mkRecoveryCode "123456789ABCDEF01234") `shouldBe` Just (recoveryCodeText knownCode),
+                 map (isNothing . mkRecoveryCode) ["", "12345-6789A-BCDEF-0123", "12345-6789A-BCDEF-012345", "12345-6789A-BCDEF-0123G"] `shouldBe` [True, True, True, True]
+               ]
+        )
       generatedCode <- generateRecoveryCode
       fmap recoveryCodeText (mkRecoveryCode (recoveryCodeText generatedCode)) `shouldBe` Just (recoveryCodeText generatedCode)
 
@@ -27,11 +32,14 @@ spec = do
     it "stores an Argon2id verifier without retaining the recovery code" $ do
       let maybeHash = hashRecoveryCodeWithSalt defaultPasswordHashingPolicy (ByteString.replicate 16 1) knownCode
           hashValue = required maybeHash
-      Text.isInfixOf "12345" (recoveryCodeHashText hashValue) `shouldBe` False
-      fmap recoveryCodeHashText (readRecoveryCodeHash (recoveryCodeHashText hashValue)) `shouldBe` Just (recoveryCodeHashText hashValue)
-      isNothing (readRecoveryCodeHash "not-an-argon2id-hash") `shouldBe` True
-      verifyRecoveryCode knownCode hashValue `shouldBe` True
-      verifyRecoveryCode otherCode hashValue `shouldBe` False
+      expectAll
+        ( (Text.isInfixOf "12345" (recoveryCodeHashText hashValue) `shouldBe` False)
+            :| [ fmap recoveryCodeHashText (readRecoveryCodeHash (recoveryCodeHashText hashValue)) `shouldBe` Just (recoveryCodeHashText hashValue),
+                 isNothing (readRecoveryCodeHash "not-an-argon2id-hash") `shouldBe` True,
+                 verifyRecoveryCode knownCode hashValue `shouldBe` True,
+                 verifyRecoveryCode otherCode hashValue `shouldBe` False
+               ]
+        )
       maybeRandomHash <- hashRecoveryCode defaultPasswordHashingPolicy knownCode
       fmap (verifyRecoveryCode knownCode) maybeRandomHash `shouldBe` Just True
 
