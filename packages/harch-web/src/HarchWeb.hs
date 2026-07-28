6efb40383b9f782d42815e29c6cdf5f9106e2860
David Bruck <1393236+gorban@users.noreply.github.com>
2026-07-27 20:10:13 -0500

    Extract ACME response decoders

--- packages/harch-web/src/HarchWeb.hs
+++ packages/harch-web/src/HarchWeb.hs
@@ -251,6 +251,13 @@ import HarchWeb.Acme.Json
     parseJsonValue,
     unicodeJsonCharacterParser,
   )
+import HarchWeb.Acme.Protocol.Decode
+  ( parseAcmeAuthorizationResponse,
+    parseAcmeChallengeResponse,
+    parseAcmeDirectoryResponse,
+    parseAcmeOrderIdentifier,
+    parseAcmeOrderResponse,
+  )
 import HarchWeb.Acme.Protocol.Types
   ( AcmeAuthorizationResponse (..),
     AcmeChallengeResponse (..),
@@ -714,45 +721,6 @@ toRuntimeWaiApplication challengeStore webApplication request respond = do
         challengeResponse
       respond challengeResponse
     Nothing -> toWaiApplication webApplication request respond
-
-parseAcmeDirectoryResponse :: JsonValue -> Either String AcmeDirectoryResponse
-parseAcmeDirectoryResponse value = do
-  fields <- jsonObjectFields "AcmeDirectoryResponse" value
-  AcmeDirectoryResponse
-    <$> jsonRequiredTextField "newNonce" fields
-    <*> jsonRequiredTextField "newAccount" fields
-    <*> jsonRequiredTextField "newOrder" fields
-
-parseAcmeOrderIdentifier :: JsonValue -> Either String AcmeOrderIdentifier
-parseAcmeOrderIdentifier value = do
-  fields <- jsonObjectFields "AcmeOrderIdentifier" value
-  AcmeOrderIdentifier
-    <$> jsonRequiredTextField "type" fields
-    <*> jsonRequiredTextField "value" fields
-
-parseAcmeChallengeResponse :: JsonValue -> Either String AcmeChallengeResponse
-parseAcmeChallengeResponse value = do
-  fields <- jsonObjectFields "AcmeChallengeResponse" value
-  AcmeChallengeResponse
-    <$> jsonRequiredTextField "type" fields
-    <*> jsonRequiredTextField "url" fields
-    <*> jsonRequiredTextField "token" fields
-
-parseAcmeAuthorizationResponse :: JsonValue -> Either String AcmeAuthorizationResponse
-parseAcmeAuthorizationResponse value = do
-  fields <- jsonObjectFields "AcmeAuthorizationResponse" value
-  AcmeAuthorizationResponse
-    <$> (jsonRequiredField "identifier" fields >>= parseAcmeOrderIdentifier)
-    <*> (jsonRequiredField "challenges" fields >>= jsonArrayItems "challenges" >>= traverse parseAcmeChallengeResponse)
-
-parseAcmeOrderResponse :: JsonValue -> Either String AcmeOrderResponse
-parseAcmeOrderResponse value = do
-  fields <- jsonObjectFields "AcmeOrderResponse" value
-  AcmeOrderResponse
-    <$> jsonRequiredTextField "status" fields
-    <*> jsonOptionalTextArrayField "authorizations" fields
-    <*> jsonOptionalTextField "finalize" fields
-    <*> jsonOptionalTextField "certificate" fields
 
 generateAcmeAccountKey :: RuntimeAcmeBindPlan -> FilePath -> IO ()
 generateAcmeAccountKey !runtimeAcmePlan accountKeyPath =
